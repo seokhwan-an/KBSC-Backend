561ff0faf55f166d9e09fc338b4a587a370ea198
SeokHwan An <asok369258@naver.com>
2022-08-03 16:01:25 +0900

    init

---
+++ src/main/java/com/hanwul/kbscbackend/KbscBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hanwul.kbscbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbscBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbscBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanwul/kbscbackend/KbscBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanwul.kbscbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbscBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
