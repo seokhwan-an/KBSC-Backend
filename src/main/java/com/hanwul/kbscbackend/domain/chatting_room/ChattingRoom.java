1bb4a3c562b5a701acc8f1359e258c046f19aaed
SeokHwan An <asok369258@naver.com>
2022-08-16 18:34:13 +0900

    fix : type 변경

--- src/main/java/com/hanwul/kbscbackend/domain/chatting_room/ChattingRoom.java
+++ src/main/java/com/hanwul/kbscbackend/domain/chatting_room/ChattingRoom.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class ChattingRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String description;
 
