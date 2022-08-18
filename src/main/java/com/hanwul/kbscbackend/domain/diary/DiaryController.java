61b60918e3c0bb9dff875956d71b4424c18f5ec1
Joung Min Ju <alswn9938@gmail.com>
2022-08-18 14:41:06 +0900

    feat : add DiaryController

--- src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryController.java
+++ src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryController.java
@@ -1,10 +1,58 @@
 package com.hanwul.kbscbackend.domain.diary;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/diary")
 @RestController
 public class DiaryController {
 
+    private final DiaryRepository diaryRepository;
+
+    @GetMapping("")
+    public List<Diary> getAllPosts() {
+        List<Diary> posts = diaryRepository.findAll();
+        return posts;
+    }
+
+    @PostMapping("")
+    public void createPost(@RequestBody DiaryDto diaryDto){
+        // DiaryDto -> Diary 로직
+        // diaryRepository.save();
+        // redirect readPost
+    }
+
+    @GetMapping("/{diaryId}")
+    public Optional<Diary> readPost(@PathVariable Long diaryId) {
+        Optional<Diary> diary = diaryRepository.findById(diaryId);
+        return diary;
+    }
+
+    @PatchMapping("/{diaryId}")
+    public void updatePost(@PathVariable Long diaryId, @RequestBody DiaryDto diaryDto){
+        Optional<Diary> diary = diaryRepository.findById(diaryId);
+        // DiaryDto -> Diary update 로직
+        // redirect readPost
+    }
+
+    @DeleteMapping("/{diaryId}")
+    public void deletePost(@PathVariable Long diaryId){
+        Optional<Diary> diary = diaryRepository.findById(diaryId);
+        diaryRepository.delete(diary.get());
+        // redirect getAllPosts
+    }
+
+    // 좋아요
+    @GetMapping("/{diaryId}/like")
+    public void likePost(@PathVariable Long diaryId){
+        Optional<Diary> diary = diaryRepository.findById(diaryId);
+        // diary 좋아요 로직
+        // redirect readPost
+    }
 }
