b84e5326581ddce1c13e3f2236141094f2d4c5a8
Joung Min Ju <alswn9938@gmail.com>
2022-08-18 11:08:11 +0900

    fix : extends JpaRepository & change return type

---
+++ src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryController.java
@@ -0,0 +1,10 @@
+package com.hanwul.kbscbackend.domain.diary;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/diary")
+@RestController
+public class DiaryController {
+
+}
--- src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryRepository.java
+++ src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
-    Optional<Status> findByStatus(Status status);
+    Optional<Diary> findByStatus(Status status);
 
-    Optional<User> findByUser(User user);
+    Optional<Diary> findByUser(User user);
 }
