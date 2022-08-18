6b0fc39883f41af98fa18a77d851be21058cb81f
Joung Min Ju <alswn9938@gmail.com>
2022-08-18 14:40:33 +0900

    feat : add DiaryDto

---
+++ src/main/java/com/hanwul/kbscbackend/domain/diary/DiaryDto.java
@@ -0,0 +1,31 @@
+package com.hanwul.kbscbackend.domain.diary;
+
+import com.hanwul.kbscbackend.domain.diarylike.DiaryLike;
+import com.hanwul.kbscbackend.domain.user.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiaryDto {
+    private Long id;
+
+    private String Content;
+
+    private Status status;
+
+    private LocalDateTime createdDateTime;
+
+    private LocalDateTime modifiedDateTime;
+
+    private List<DiaryLike> diaryLikeList = new ArrayList<>();
+
+    private User user;
+}
+
