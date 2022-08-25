3ded1993dd16142fd0a7300ebcb119628de9fdb7
MINJU <alswn9938@gmail.com>
2022-08-25 16:43:17 +0900

    feat : add Mission Repository

---
+++ src/main/java/com/hanwul/kbscbackend/domain/mission/MissionRepository.java
@@ -0,0 +1,6 @@
+package com.hanwul.kbscbackend.domain.mission;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+}
