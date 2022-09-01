71d06b6f97764d008dc375842d68c2bb35773b78
SeokHwan An <asok369258@naver.com>
2022-09-02 02:55:34 +0900

    refactor : user에서 account로 변함으로 인한 코드 수정

--- src/main/java/com/hanwul/kbscbackend/domain/answer/AnswerRepository.java
+++ src/main/java/com/hanwul/kbscbackend/domain/answer/AnswerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    Optional<Answer> findByUser(Account user);
+    Optional<Answer> findByAccount(Account user);
 
     Optional<Question> findByQuestion(Question question);
 }
--- src/main/java/com/hanwul/kbscbackend/domain/chatting_room/ChattingRoomRepository.java
+++ src/main/java/com/hanwul/kbscbackend/domain/chatting_room/ChattingRoomRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
-    Optional<Account> findByConstructor(Account user);
-
-    Optional<Account> findByParticipant(Account user);
+//    Optional<Account> findByConstructor(Account user);
+//
+//    Optional<Account> findByParticipant(Account user);
 }
