640648eeec8ee137b011c30f7d158421e5ce440c
SeokHwan An <asok369258@naver.com>
2022-09-06 10:13:13 +0900

    refactor : 비밀번호 8자리 이상 숫자 또는 특수문자 1개 포함 정규식 추가

--- src/main/java/com/hanwul/kbscbackend/domain/account/SignUpDto.java
+++ src/main/java/com/hanwul/kbscbackend/domain/account/SignUpDto.java
@@ -1,11 +1,11 @@
 package com.hanwul.kbscbackend.domain.account;
 
 
-
 import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor
@@ -14,12 +14,13 @@ public class SignUpDto {
     @NotBlank
     private String username;
     @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])((?=.*[~!@#$%^&*()_+=])|(?=.*\\d))[A-Za-z\\d~!@#$%^&*()_+=]{8,}$")
     private String password;
     @NotBlank
     private String nickname;
 
     @Builder
-    public SignUpDto(String username, String password, String nickname){
+    public SignUpDto(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
