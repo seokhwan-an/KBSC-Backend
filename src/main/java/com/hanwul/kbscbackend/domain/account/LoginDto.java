9065db0810a409b3c9ee1eed8151704df4847a88
SeokHwan An <asok369258@naver.com>
2022-09-06 10:12:31 +0900

    refactor : role에 대한 필드 추가

--- src/main/java/com/hanwul/kbscbackend/domain/account/LoginDto.java
+++ src/main/java/com/hanwul/kbscbackend/domain/account/LoginDto.java
@@ -5,7 +5,11 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -16,8 +20,11 @@ public class LoginDto {
     @NotBlank
     private String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
     @Builder
-    public LoginDto(String username, String password){
+    public LoginDto(String username, String password) {
         this.username = username;
         this.password = password;
     }
