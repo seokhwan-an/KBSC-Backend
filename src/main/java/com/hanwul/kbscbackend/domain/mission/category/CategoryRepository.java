08be523ff0134b581b07e009207dcaad93304f86
SeokHwan An <asok369258@naver.com>
2022-09-14 21:59:27 +0900

    refactor : 필요없는 코드 제거

--- src/main/java/com/hanwul/kbscbackend/domain/mission/category/CategoryRepository.java
+++ src/main/java/com/hanwul/kbscbackend/domain/mission/category/CategoryRepository.java
@@ -2,7 +2,5 @@ package com.hanwul.kbscbackend.domain.mission.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
