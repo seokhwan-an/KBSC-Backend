d68d66013eeb3eef5bcbf1b6f9ccbb9331b44913
SeokHwan An <asok369258@naver.com>
2022-09-14 21:55:13 +0900

    refactor : 미션 카테고리 종류 추가

--- src/main/java/com/hanwul/kbscbackend/domain/mission/MissionCategory.java
+++ src/main/java/com/hanwul/kbscbackend/domain/mission/MissionCategory.java
@@ -2,8 +2,11 @@ package com.hanwul.kbscbackend.domain.mission;
 
 public enum MissionCategory {
     INSOMNIA("불면증"),
+    CONFIDENCE("자신감 회복"),
     FEAR("불안감"),
-    ANOREXIA("식욕 부진");
+    ANOREXIA("식욕 부진"),
+    HELPLESS("무력감");
+
 
     private String korean;
 
