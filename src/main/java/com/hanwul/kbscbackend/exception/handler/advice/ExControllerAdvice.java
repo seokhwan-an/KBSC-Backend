6215ec8f08e049bdec12456b7921276874f61aa3
SeokHwan An <asok369258@naver.com>
2022-09-14 21:54:07 +0900

    refactor : 오류 핸들러 메소트 추가 및 수정

--- src/main/java/com/hanwul/kbscbackend/exception/handler/advice/ExControllerAdvice.java
+++ src/main/java/com/hanwul/kbscbackend/exception/handler/advice/ExControllerAdvice.java
@@ -44,8 +44,9 @@ public class ExControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NotMyAnswer.class)
-    public ErrorResult notMyAnswer(IllegalArgumentException e){
+    public ErrorResult notMyAnswer(IllegalArgumentException e) {
         return new ErrorResult(ExceptionCode.NOT_MY_ANSWER);
+    }
     @ExceptionHandler(WrongEmotionId.class)
     public ErrorResult wrongEmotionId(IllegalArgumentException e){
         return new ErrorResult(ExceptionCode.NOT_FOUND_EMOTION);
@@ -64,4 +65,15 @@ public class ExControllerAdvice {
 
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongMatchMission.class)
+    public ErrorResult wrongMatchMission(IllegalArgumentException e){
+        return new  ErrorResult(ExceptionCode.WRONG_MATCH_MISSION);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongCategoryId.class)
+    public ErrorResult wrongCategoryId(IllegalArgumentException e){
+        return new ErrorResult(ExceptionCode.WRONG_CATEGORY_ID);
+    }
 }
